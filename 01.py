7093a2b397d1e9fbd6b00d480ade2c48856317d8
Rajiv Giridharagopal <rgiri@uw.edu>
2022-11-30 23:02:59 -0800

    Add files via upload

--- 01.py
+++ 01.py
@@ -23,3 +23,18 @@ print('Max elf:', max(elves))
 # Part 2: Find top 3
 elves.sort()
 print('Top 3 sum:', sum(elves[-3:]))
+
+# Alternate to learn: using map function
+f = open(base + r'01_input.txt')
+data = f.read().split('\n\n')
+
+elves = []
+for d in data:
+    elves.append(tuple([int(x) for x in d.strip().split('\n')]))
+
+elfsum = list(map(sum, elves))
+print(max(elfsum))
+
+elves.sort(key=sum, reverse=True)
+elfsum = list(map(sum, elves))
+print(sum(elfsum[:3]))
