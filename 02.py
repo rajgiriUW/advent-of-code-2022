73e569041175c5e0c17b737b5ef5f9d7e847eeab
Rajiv Giridharagopal <rgiri@uw.edu>
2022-12-01 22:18:08 -0800

    Add files via upload

--- 02.py
+++ 02.py
@@ -3,27 +3,19 @@ f = open(base + r'02_input.txt')
 data = [x.strip() for x in f.readlines()]
 
 # Part 1
-pairs1 = {'AX':4, 'AY':8, 'AZ':3, 
-         'BX':1, 'BY':5, 'BZ':9, 
-         'CX':7, 'CY':2, 'CZ':6}
+pairs1 = {'A X':4, 'A Y':8, 'A Z':3, 
+         'B X':1, 'B Y':5, 'B Z':9, 
+         'C X':7, 'C Y':2, 'C Z':6}
 
-score = 0
-for d in data:
-    
-    p = ''.join(d.split(' '))
-    score += pairs1[p]
+score = sum([pairs1[x] for x in data])
     
 print('Round 1 Score:', score)
 
 # Part 2
-pairs2 = {'AX':3, 'AY':4, 'AZ':8, 
-         'BX':1, 'BY':5, 'BZ':9, 
-         'CX':2, 'CY':6, 'CZ':7}
+pairs2 = {'A X':3, 'A Y':4, 'A Z':8, 
+         'B X':1, 'B Y':5, 'B Z':9, 
+         'C X':2, 'C Y':6, 'C Z':7}
 
-score = 0
-for d in data:
-    
-    p = ''.join(d.split(' '))
-    score += pairs2[p]
+score = sum([pairs2[x] for x in data])
     
 print('Round 2 Score:', score)
